382941da5cba8786d43e269522e8b1d1658a4c83
Seth Glenn <Sbgees@yahoo.com>
2020-10-05 23:43:52 -0500

    Adding highscores html page and implementing JS.

--- 04_code_quiz/script.js
+++ 04_code_quiz/script.js
@@ -1,4 +1,4 @@
-// declaring variables
+// declaring variables and Arrays
 var secondsLeft = 120;
 
 var timer = document.querySelector("#timer");
@@ -13,6 +13,8 @@ var choiceD = document.querySelector("#d");
 
 
 
+var btnAdvance = [choiceA, choiceB, choiceC, choiceD];
+
 // Questions object
 var questions = {
     "question set 1": [
@@ -23,10 +25,20 @@ var questions = {
         "None of the above"
     ],
     "question set 2" : [
-        
+        "What does JS mean?",
+        "Just Study",
+        "John Stamos",
+        "July September",
+        "Java Script"
 
     ],
+    "question set 3" :[
+        "test",
+        "test",
+        "test",
+        "test"
 
+    ]
 
     
 
@@ -47,37 +59,48 @@ function startTimer(){
 }
 // Function that starts quiz
 function quizStart(event){
-    event.preventDefault();
-    preQuiz.style.display = "none";
-    preQuizQuestions.style.display = "block";
-    quizQuestions.textContent = (questions["question set 1"][0]);
-    choiceA.textContent = (questions["question set 1"][1]);
-    choiceB.textContent = (questions["question set 1"][2]);
-    choiceC.textContent = (questions["question set 1"][3]);
-    choiceD.textContent = (questions["question set 1"][4]);
+    if (event.target.matches("#btnStart")) {  
+        event.preventDefault();
+        preQuiz.style.display = "none";
+        preQuizQuestions.style.display = "block";
+        quizQuestions.textContent = (questions["question set 1"][0]);
+        choiceA.textContent = (questions["question set 1"][1]);
+        choiceB.textContent = (questions["question set 1"][2]);
+        choiceC.textContent = (questions["question set 1"][3]);
+        choiceD.textContent = (questions["question set 1"][4]);
 
     startTimer()
 
-
+    }
    
-        
-
-    
-    
 }
+// Function(s) that advances quiz through question sets
+function quizAdvance(event){
+    event.preventDefault();
+    quizQuestions.textContent = (questions["question set 2"][0]);
+    choiceA.textContent = (questions["question set 2"][1]);
+    choiceB.textContent = (questions["question set 2"][2]);
+    choiceC.textContent = (questions["question set 2"][3]);
+    choiceD.textContent = (questions["question set 2"][4]);  
+}
+// function quizAdvance2(event){
+//     event.preventDefault();
+//     quizQuestions.textContent = (questions["question set 3"][0]);
+//     choiceA.textContent = (questions["question set 3"][1]);
+//     choiceB.textContent = (questions["question set 3"][2]);
+//     choiceC.textContent = (questions["question set 3"][3]);
+//     choiceD.textContent = (questions["question set 3"][4]);
+
+// }
 
 
 
-
-
-
-
-
-
-
+// Event listeners
 btnStart.addEventListener("click", quizStart);
 
-
+for (let i = 0; i <= btnAdvance.length; i++){
+     btnAdvance[i].addEventListener("click", quizAdvance);   
+}
 
 
 
